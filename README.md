267dd6ba52fbc0668c3f3eae17cfb336b8e970b2
amigdol1 <alex.migdol@gmail.com>
2016-06-22 23:07:26 -0700

    add basic html framework, js variable for inputting a number into a form, and BDD in the README

---
+++ README.md
@@ -0,0 +1,29 @@
+# Ping-Pong: A Range Tool with logic
+## Loops and Arrays: Introduction to Programming (Evening Course) 6.26.2016-06
+
+## Description
+
+## Expected Behavior Examples
+1. List range of numbers starting from 1 to what the user inputs
+  Input Example: 2
+  Output Example: 1, 2
+2. Numbers divisible by 3 will display as "ping"
+  Input Example: 3
+  Output Example: ping
+3. Numbers divisible by 5 will display as "pong"
+  Input Example: 5
+  Output Example: pong
+4. Numbers divisible by 15 will display as "pingpong"
+  Input Example: 15
+  Output Example: pingpong
+
+
+
+## Setup
+
+## Technologies Used
+1. HTML
+2. CSS
+3. Javascript
+4. Bootstrap CSS
+5. Jquery
---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+
+
+// var outputNumber = single item in array that is in the output (one of the array values)
+
+
+/* if (outputNumber % 3 === 0) {
+  return "Ping" // most likely need to use replace method here
+}
+else if (outputNumber % 5 ===0) {
+  return "Pong" // most likely need to use replace method here
+}
+else if (outputNumber % 15 ===0) {
+  return "PingPong" // most likely need to use replace method here
+} */
+
+// will use .range to get a range of numbers from 1 to input variable a
+
+// this is where the front-end logic will start
+
+$(document).ready(function(){
+  $("form#ping-pong").submit(function(event){
+    event.preventDefault();
+    var input = parseInt($("input#number").val());
+    console.log(input)
+  })
+});
