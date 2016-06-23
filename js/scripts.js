0a3b20e02708d963598ade1646a7531e3d83d890
amigdol1 <alex.migdol@gmail.com>
2016-06-23 01:23:50 -0700

    add js functionality for displaying range using a for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,35 @@
 
+// var outputNumber = a number within the array "range"
 
+var pingPong = function(outputNumber) {
+  if (outputNumber % 3 === 0) {
+    return "ping" // most likely need to use replace method here
+  }
+  else if (outputNumber % 5 === 0) {
+    return "pong" // most likely need to use replace method here
+  }
+  else if (outputNumber % 15 === 0) {
+    return "pingpong" // most likely need to use replace method here
+  }
+  console.log(pingPong(outputNumber));
 
+};
 
-
-
-// var outputNumber = single item in array that is in the output (one of the array values)
-
-
-/* if (outputNumber % 3 === 0) {
-  return "Ping" // most likely need to use replace method here
-}
-else if (outputNumber % 5 ===0) {
-  return "Pong" // most likely need to use replace method here
-}
-else if (outputNumber % 15 ===0) {
-  return "PingPong" // most likely need to use replace method here
-} */
-
-// will use .range to get a range of numbers from 1 to input variable a
 
 // this is where the front-end logic will start
 
-$(document).ready(function(){
+$(document).ready(function() {
   $("form#ping-pong").submit(function(event){
     event.preventDefault();
     var input = parseInt($("input#number").val());
     console.log(input)
 
-    var range = Array.apply(1, Array(input + 1)).map(function (_, input) {
-      return input;
-    });
+    var totle = 1
+    var range = [];
+
+    for (var index = 1; index <= input; index += 1) {
+      range.push(index);
+    };
     console.log(range)
-  })
+  });
 });
