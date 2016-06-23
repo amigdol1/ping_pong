32346467161a765651669a09690ea1e8977c866b
amigdol1 <alex.migdol@gmail.com>
2016-06-23 13:45:41 -0700

    add full logic to display ping/pong/pingpong on 3, 5, and 15 while listing the other numbers as is

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,3 @@
-
-// output= any number within the array "range" - need to still create this
-
-
-// http://stackoverflow.com/questions/16881872/changing-array-value-when-condition-is-met-in-javascript
-// this is where the front-end logic will start
-
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event){
     event.preventDefault();
@@ -20,14 +13,17 @@ $(document).ready(function() {
     console.log(range)
 
     var pingPong = $.map(range, function(output) {
-      if (output % 3 === 0) {
-        return "ping" // most likely need to use replace method here
+      if (output % 15 === 0) {
+        return "pingpong"
       }
       else if (output % 5 === 0) {
-        return "pong" // most likely need to use replace method here
+        return "pong"
       }
-      else if (output % 15 === 0) {
-        return "pingpong" // most likely need to use replace method here
+      else if (output % 3 === 0) {
+      return "ping"
+      }
+      else {
+        return output;
       }
     });
     console.log(pingPong);
