2b15981c396c19c3d1768cbd405de3d56f8437a9
amigdol1 <alex.migdol@gmail.com>
2016-06-23 13:33:14 -0700

    add logic to recognize when number is divisble by 3, 5, and 15.  It will still need the output of replacing the numbers and listing them all in chronological order

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,8 @@
 
-// var outputNumber = a number within the array "range"
-
-var pingPong = function(outputNumber) {
-  if (outputNumber % 3 === 0) {
-    return "ping" // most likely need to use replace method here
-  }
-  else if (outputNumber % 5 === 0) {
-    return "pong" // most likely need to use replace method here
-  }
-  else if (outputNumber % 15 === 0) {
-    return "pingpong" // most likely need to use replace method here
-  }
-  console.log(pingPong(outputNumber));
-
-};
+// output= any number within the array "range" - need to still create this
 
 
+// http://stackoverflow.com/questions/16881872/changing-array-value-when-condition-is-met-in-javascript
 // this is where the front-end logic will start
 
 $(document).ready(function() {
@@ -31,5 +18,18 @@ $(document).ready(function() {
       range.push(index);
     };
     console.log(range)
+
+    var pingPong = $.map(range, function(output) {
+      if (output % 3 === 0) {
+        return "ping" // most likely need to use replace method here
+      }
+      else if (output % 5 === 0) {
+        return "pong" // most likely need to use replace method here
+      }
+      else if (output % 15 === 0) {
+        return "pingpong" // most likely need to use replace method here
+      }
+    });
+    console.log(pingPong);
   });
 });
