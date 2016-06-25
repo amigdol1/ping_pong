feeb040fb5aa1ad12e4850538b7d4d59e53a7a59
amigdol1 <alex.migdol@gmail.com>
2016-06-25 14:26:09 -0700

    sucessfully put business logic outside of front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,27 @@
+var number = function(output) {
+  if (output % 15 === 0) {
+    return "pingpong"
+  } else if (output % 5 === 0) {
+    return "pong"
+  } else if (output % 3 === 0) {
+  return "ping"
+  } else {
+    return output;
+  }
+}
+
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event){
     event.preventDefault();
     var input = parseInt($("input#number").val());
-    console.log(input)
 
     var range = [];
     for (var index = 1; index <= input; index += 1) {
       range.push(index);
     };
-    console.log(range)
 
-    var pingPong = $.map(range, function(output) {
-      if (output % 15 === 0) {
-        return "pingpong"
-      } else if (output % 5 === 0) {
-        return "pong"
-      } else if (output % 3 === 0) {
-      return "ping"
-      } else {
-        return output;
-      }
-    });
-
+    var pingPong = $.map(range, number)
     var result = pingPong.join(',  ')
-    console.log(result);
 
     $("#result").text(result)
     });
